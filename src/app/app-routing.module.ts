99b875fd0964dbe7027eafefa1b8a9cc526c0f1a
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-11-11 11:18:55 +0100

    app Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {BookingComponent} from './components/booking/booking.component';
+import {InstallationsComponent} from './components/installations/installations.component';
+import {FacilitiesComponent} from './components/facilities/facilities.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'installations', component: InstallationsComponent },
+  { path: 'facilities', component: FacilitiesComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
