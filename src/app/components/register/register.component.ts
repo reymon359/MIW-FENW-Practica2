40f7ad204760a22c660a9bf824880fa322967c23
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-11-12 12:41:37 +0100

    email and improved ngclass

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -10,11 +10,12 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
 
-
   constructor() {
     this.registerForm = new FormGroup({
       userId: new FormControl('',
         [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]),
     });
   }
 
