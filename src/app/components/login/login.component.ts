1bc98d4fcf2c861a28389f5f45baffe89f74e976
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-11-25 10:28:40 +0100

    changed directories structure

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {UserService} from '../../services/user/user.service';
+import {UserService} from '../../shared/services/user/user.service';
 
 @Component({
   selector: 'app-login',
