ef5ce6a635e58d67b8e278724e3d9f8a78470d52
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-11-27 12:26:57 +0100

    booking template done

--- src/app/shared/services/reservation/reservation.service.ts
+++ src/app/shared/services/reservation/reservation.service.ts
@@ -32,7 +32,7 @@ export class ReservationService {
     const url = `${this.url}reservations`;
     const headers = this.getHeaders();
     return this.http.get(url, {headers, observe: 'response'})
-      .pipe(map(data => console.log(data)));
+      .pipe(map(data => data));
   }
 
 
